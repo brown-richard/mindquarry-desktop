1f7feaeb525315a7bf7ba5d05493ad17b29f187f
alexs <alexs@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-02-05 11:58:09 +0000

    - refactored HTTP util - fixed 302 problem when tasks are set to done

--- src/main/java/com/mindquarry/client/util/network/HttpUtil.java
+++ src/main/java/com/mindquarry/client/util/network/HttpUtil.java
@@ -15,6 +15,8 @@ package com.mindquarry.client.util.network;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
@@ -33,82 +35,87 @@ import com.mindquarry.client.util.widgets.MessageDialogUtil;
 public class HttpUtil {
     public static InputStream getContentAsXML(String login, String pwd,
             String address) throws HttpException, IOException {
-        HttpClient httpClient = new HttpClient();
-        httpClient.getState().setCredentials(
-                new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT,
-                        AuthScope.ANY_REALM),
-                new UsernamePasswordCredentials(login, pwd));
-
-        GetMethod get = new GetMethod(address);
-        get.setDoAuthentication(true);
-        get.addRequestHeader("accept", "text/xml"); //$NON-NLS-1$ //$NON-NLS-2$
-        httpClient.executeMethod(get);
+        HttpClient client = createHttpClient(login, pwd, address);
+        GetMethod get = createAndExecuteGetMethod(address, client);
 
         InputStream result = null;
         if (get.getStatusCode() == 200) {
-        	result = get.getResponseBodyAsStream();
+            result = get.getResponseBodyAsStream();
         } else if (get.getStatusCode() == 401) {
-            MessageDialogUtil
-                    .displaySyncErrorMsg(Messages.getString("HttpUtil.0")); //$NON-NLS-1$
+            MessageDialogUtil.displaySyncErrorMsg(Messages
+                    .getString("HttpUtil.0")); //$NON-NLS-1$
         } else {
-            MessageDialogUtil
-                    .displaySyncErrorMsg(Messages.getString("HttpUtil.1") //$NON-NLS-1$
-                            + get.getStatusCode());
+            MessageDialogUtil.displaySyncErrorMsg(Messages
+                    .getString("HttpUtil.1") //$NON-NLS-1$
+                    + get.getStatusCode());
         }
-        //get.releaseConnection();
         return result;
     }
-    
+
     public static String getContentAsString(String login, String pwd,
             String address) throws HttpException, IOException {
-        HttpClient httpClient = new HttpClient();
-        httpClient.getState().setCredentials(
-                new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT,
-                        AuthScope.ANY_REALM),
-                new UsernamePasswordCredentials(login, pwd));
-
-        GetMethod get = new GetMethod(address);
-        get.setDoAuthentication(true);
-        get.addRequestHeader("accept", "text/xml"); //$NON-NLS-1$ //$NON-NLS-2$
-        httpClient.executeMethod(get);
+        HttpClient client = createHttpClient(login, pwd, address);
+        GetMethod get = createAndExecuteGetMethod(address, client);
 
         String result = null;
         if (get.getStatusCode() == 200) {
             result = get.getResponseBodyAsString();
         } else if (get.getStatusCode() == 401) {
-            MessageDialogUtil
-                    .displaySyncErrorMsg(Messages.getString("HttpUtil.0")); //$NON-NLS-1$
+            MessageDialogUtil.displaySyncErrorMsg(Messages
+                    .getString("HttpUtil.0")); //$NON-NLS-1$
         } else {
-            MessageDialogUtil
-                    .displaySyncErrorMsg(Messages.getString("HttpUtil.1") //$NON-NLS-1$
-                            + get.getStatusCode());
+            MessageDialogUtil.displaySyncErrorMsg(Messages
+                    .getString("HttpUtil.1") //$NON-NLS-1$
+                    + get.getStatusCode());
         }
-        //get.releaseConnection();
         return result;
     }
 
     public static void putAsXML(String login, String pwd, String address,
             byte[] content) throws HttpException, IOException {
-        HttpClient httpClient = new HttpClient();
-        httpClient.getState().setCredentials(
-                new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT,
-                        AuthScope.ANY_REALM),
-                new UsernamePasswordCredentials(login, pwd));
+        HttpClient client = createHttpClient(login, pwd, address);
 
         PutMethod put = new PutMethod(address);
         put.setDoAuthentication(true);
+        put.addRequestHeader("accept", "text/xml"); //$NON-NLS-1$ //$NON-NLS-2$
         put.setRequestEntity(new ByteArrayRequestEntity(content));
 
-        httpClient.executeMethod(put);
+        client.executeMethod(put);
 
         if (put.getStatusCode() == 401) {
-            MessageDialogUtil
-                    .displaySyncErrorMsg(Messages.getString("HttpUtil.0")); //$NON-NLS-1$
+            MessageDialogUtil.displaySyncErrorMsg(Messages
+                    .getString("HttpUtil.0")); //$NON-NLS-1$
+        } else if (put.getStatusCode() == 302) {
+            // we received a redirect to the URL of the putted document, so
+            // everthign seems right and we have nothing to do
         } else if (put.getStatusCode() != 200) {
-            MessageDialogUtil
-                    .displaySyncErrorMsg(Messages.getString("HttpUtil.1") //$NON-NLS-1$
-                            + put.getStatusCode());
+            MessageDialogUtil.displaySyncErrorMsg(Messages
+                    .getString("HttpUtil.1") //$NON-NLS-1$
+                    + put.getStatusCode());
         }
         put.releaseConnection();
+    }
+
+    private static HttpClient createHttpClient(String login, String pwd,
+            String address) throws MalformedURLException {
+        URL url = new URL(address);
+
+        HttpClient client = new HttpClient();
+        client.getParams().setAuthenticationPreemptive(true);
+        client.getState()
+                .setCredentials(
+                        new AuthScope(url.getHost(), url.getPort(),
+                                AuthScope.ANY_REALM),
+                        new UsernamePasswordCredentials(login, pwd));
+        return client;
+    }
+
+    private static GetMethod createAndExecuteGetMethod(String address,
+            HttpClient client) throws IOException, HttpException {
+        GetMethod get = new GetMethod(address);
+        get.setDoAuthentication(true);
+        get.addRequestHeader("accept", "text/xml"); //$NON-NLS-1$ //$NON-NLS-2$
+        client.executeMethod(get);
+        return get;
     }
 }
