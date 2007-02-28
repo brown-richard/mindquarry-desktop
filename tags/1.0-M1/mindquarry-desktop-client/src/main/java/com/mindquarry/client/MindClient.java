0850d55455e5b63a2ec9677d4e7c37b254cfd3cd
lars <lars@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-02-28 12:36:33 +0000

    created tag

---
+++ tags/1.0-M1/mindquarry-desktop-client/src/main/java/com/mindquarry/client/MindClient.java
@@ -0,0 +1,335 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.GetTrayItemLocationHackMacOSX;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.ToolTip;
+import org.eclipse.swt.widgets.Tray;
+import org.eclipse.swt.widgets.TrayItem;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.mindquarry.client.ballon.MindClientBallonWidget;
+import com.mindquarry.client.options.Profile;
+import com.mindquarry.client.options.ProfileList;
+import com.mindquarry.client.options.dialog.OptionsDialog;
+import com.mindquarry.client.util.os.OperatingSystem;
+import com.mindquarry.client.util.widgets.IconActionThread;
+import com.mindquarry.client.workspace.WorkspaceSynchronizeListener;
+
+/**
+ * Main class for the MindClient. Responsible for managing persistence of
+ * options and creation of initial GUI types.
+ * 
+ * @author <a href="mailto:lars(dot)trieloff(at)mindquarry(dot)com">Lars
+ *         Trieloff</a>
+ * @author <a href="mailto:alexander(dot)saar(at)mindquarry(dot)com">Alexander
+ *         Saar</a>
+ */
+public class MindClient {
+    public static final String APPLICATION_NAME = "Mindquarry Desktop Client"; //$NON-NLS-1$
+
+    public static final String MINDCLIENT_SETTINGS = System
+            .getProperty("user.home") //$NON-NLS-1$
+            + "/.mindclient/mindclient.settings"; //$NON-NLS-1$
+
+    public static final String MINDCLIENT_ICON = "/com/mindquarry/icons/16x16/logo/mindquarry-icon.png"; //$NON-NLS-1$
+
+    public static final OperatingSystem OS = getOperatingSystem();
+
+    private static Shell shell;
+
+    private final Image icon;
+
+    private final BeanFactory factory;
+
+    private ProfileList profileList;
+
+    private File optionsFile;
+    
+    private static TrayItem item;
+    
+    private static IconActionThread iconAction;
+
+    public MindClient() {
+        factory = new ClassPathXmlApplicationContext(
+                new String[] { "applicationContext.xml" }); //$NON-NLS-1$
+
+        icon = new Image(Display.getCurrent(), getClass().getResourceAsStream(
+                MINDCLIENT_ICON));
+
+        // init settings file & name
+        profileList = new ProfileList();
+        optionsFile = new File(MINDCLIENT_SETTINGS);
+    }
+
+    public static void main(String[] args) throws IOException {
+        // init display & shell
+        Display display = new Display();
+        Display.setAppName(APPLICATION_NAME);
+        display.setWarnings(true);
+
+        shell = new Shell(display, SWT.NONE);
+        shell.setText(APPLICATION_NAME);
+
+        MindClient mindclient = new MindClient();
+        mindclient.checkArguments(args);
+        mindclient.createTrayIconAndMenu(display);
+
+        while (!display.isDisposed()) {
+            if (!display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+        display.dispose();
+    }
+
+    private void checkArguments(String[] args) throws IOException {
+        if ((args.length == 3) && (optionsFile.exists())) {
+            loadOptions();
+            addNewProfile(args[0], args[1], args[2]);
+            showOptionsDlg();
+        } else if ((args.length == 3) && (!optionsFile.exists())) {
+            addNewProfile(args[0], args[1], args[2]);
+            showOptionsDlg();
+        } else if (!optionsFile.exists()) {
+            addNewProfile(Messages.getString("MindClient.8"), "http://server", //$NON-NLS-1$ //$NON-NLS-2$
+                    "your login name"); //$NON-NLS-1$
+            showOptionsDlg();
+        } else {
+            loadOptions();
+        }
+    }
+
+    private boolean addNewProfile(String name, String endpoint, String login) {
+        // add new profile entry
+        Profile profile = new Profile();
+        profile.setName(name);
+        profile.setEndpoint(endpoint);
+        profile.setLogin(login);
+        profile.setPassword(""); //$NON-NLS-1$
+        profile.setLocation(""); //$NON-NLS-1$
+        return profileList.addProfile(profile);
+    }
+
+    private void createTrayIconAndMenu(Display display) {
+        Tray tray = display.getSystemTray();
+
+        item = new TrayItem(tray, SWT.NONE);
+        item.setImage(icon);
+
+        final MindClientBallonWidget ballonWindow = new MindClientBallonWidget(
+                display, this, item);
+
+        final Menu menu = new Menu(shell, SWT.POP_UP);
+        if (MindClient.getOperatingSystem() == OperatingSystem.MAC_OS_X) {
+            // Mac does not use the right mouse for tray icons
+
+            // left-click => menu
+            item.addSelectionListener(new SelectionListener() {
+                // single-click
+                public void widgetSelected(SelectionEvent e) {
+                    menu.setLocation(GetTrayItemLocationHackMacOSX
+                            .getAlignedLocation(item));
+                    menu.setVisible(true);
+                }
+
+                // double-click
+                public void widgetDefaultSelected(SelectionEvent e) {
+                }
+            });
+            // extra item in menu => balloon window
+            MenuItem menuItem = new MenuItem(menu, SWT.PUSH);
+            menuItem.setText(Messages.getString("MindClient.9")); //$NON-NLS-1$
+            menuItem.addListener(SWT.Selection, new Listener() {
+                public void handleEvent(Event event) {
+                    ballonWindow.toggleBalloon();
+                }
+            });
+        } else {
+            // Windows/Gnome
+
+            // right-click / context menu => menu
+            item.addListener(SWT.MenuDetect, new Listener() {
+                public void handleEvent(Event event) {
+                    menu.setVisible(true);
+                }
+            });
+
+            // left-click => balloon window
+            item.addListener(SWT.Selection, new Listener() {
+                public void handleEvent(final Event event) {
+                    ballonWindow.handleEvent(event);
+                }
+            });
+        }
+        // go to webpage
+        MenuItem menuItem = new MenuItem(menu, SWT.PUSH);
+        menuItem.setText(Messages.getString("MindClient.10")); //$NON-NLS-1$
+        menuItem.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+                if (profileList.selectedProfile() != null) {
+                    Program.launch(profileList.selectedProfile().getEndpoint());
+                }
+            }
+        });
+        // synchronize
+        menuItem = new MenuItem(menu, SWT.PUSH);
+        menuItem.setText(Messages.getString("MindClient.11")); //$NON-NLS-1$
+        menuItem.addListener(SWT.Selection, WorkspaceSynchronizeListener
+                .getInstance(this, menuItem, null));
+
+        // add separator
+        menuItem = new MenuItem(menu, SWT.SEPARATOR);
+
+        // options dialog
+        menuItem = new MenuItem(menu, SWT.PUSH);
+        menuItem.setText(Messages.getString("MindClient.0")); //$NON-NLS-1$
+        menuItem.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+                try {
+                    showOptionsDlg();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // add separator
+        menuItem = new MenuItem(menu, SWT.SEPARATOR);
+
+        // close application
+        menuItem = new MenuItem(menu, SWT.PUSH);
+        menuItem.setText(Messages.getString("MindClient.1")); //$NON-NLS-1$
+        menuItem.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+                saveOptions();
+                System.exit(1);
+            }
+        });
+        iconAction = new IconActionThread(item);
+        iconAction.setDaemon(true);
+        iconAction.start();
+    }
+
+    private void loadOptions() {
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream(
+                    optionsFile));
+            profileList = (ProfileList) is.readObject();
+            is.close();
+        } catch (Exception e) {
+            showErrorMessage(Messages.getString("MindClient.5")); //$NON-NLS-1$
+        }
+        // if no profile is selected, use the first one
+        if (profileList.selectedProfile() == null && profileList.size() > 0) {
+            profileList.select(profileList.get(0));
+        }
+    }
+
+    private void showOptionsDlg() throws IOException {
+        // request option values from user
+        OptionsDialog dlg = new OptionsDialog(MindClient.getShell(), icon,
+                profileList);
+        dlg.setBlockOnOpen(true);
+        if (dlg.open() == Window.OK) {
+            saveOptions();
+        }
+    }
+
+    public void saveOptions() {
+        try {
+            if (!optionsFile.exists()) {
+                optionsFile.getParentFile().mkdirs();
+                optionsFile.createNewFile();
+            }
+            ObjectOutputStream os = new ObjectOutputStream(
+                    new FileOutputStream(optionsFile));
+            os.writeObject(profileList);
+            os.close();
+        } catch (Exception e) {
+            showErrorMessage(Messages.getString("MindClient.7")); //$NON-NLS-1$
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        if (icon != null) {
+            icon.dispose();
+        }
+    }
+
+    public final Image getIcon() {
+        return icon;
+    }
+
+    public ProfileList getProfileList() {
+        return profileList;
+    }
+
+    public static Shell getShell() {
+        return shell;
+    }
+
+    public BeanFactory getFactory() {
+        return factory;
+    }
+
+    private static OperatingSystem getOperatingSystem() {
+        String os = System.getProperty("os.name").toLowerCase(); //$NON-NLS-1$
+        if (os.startsWith("windows")) { //$NON-NLS-1$
+            return OperatingSystem.WINDOWS;
+        } else if (os.startsWith("mac")) { //$NON-NLS-1$
+            return OperatingSystem.MAC_OS_X;
+        } else {
+            return OperatingSystem.OTHER;
+        }
+    }
+
+    public static IconActionThread getIconActionHandler() {
+        return MindClient.iconAction;
+    }
+    
+    public static void showErrorMessage(final String message) {
+        shell.getDisplay().asyncExec(new Runnable() {
+            public void run() {
+                final ToolTip tip = new ToolTip(shell, SWT.BALLOON
+                        | SWT.ICON_ERROR);
+                tip.setMessage(message);
+                tip.setText(Messages.getString("MindClient.12")); //$NON-NLS-1$
+                item.setToolTip(tip);
+                tip.setAutoHide(true);
+                tip.setVisible(true);
+            }
+        });
+    }
+}
---
+++ tags/1.0-M1/mindquarry-desktop-client/src/main/java/com/mindquarry/client/workspace/UpdateOperation.java
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.client.workspace;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.SAXReader;
+import org.tigris.subversion.javahl.ClientException;
+import org.tigris.subversion.javahl.Revision;
+
+import com.mindquarry.client.MindClient;
+import com.mindquarry.client.util.network.HttpUtil;
+import com.mindquarry.client.workspace.widgets.SynchronizeWidget;
+import com.mindquarry.client.workspace.xml.TeamListTransformer;
+import com.mindquarry.client.workspace.xml.TeamspaceTransformer;
+
+/**
+ * Updates the local working copy of the workspaces.
+ * 
+ * @author <a href="mailto:alexander(dot)saar(at)mindquarry(dot)com">Alexander
+ *         Saar</a>
+ */
+public class UpdateOperation extends SvnOperation {
+    public UpdateOperation(final MindClient client,
+            List<SynchronizeWidget> synAreas) {
+        super(client, synAreas);
+    }
+
+    /**
+     * @see java.lang.Runnable#run()
+     */
+    public void run() {
+        resetProgress();
+        HashMap<String, String> teamspaces = new HashMap<String, String>();
+        if (!getTeamspaceList(teamspaces)) {
+            resetProgress();
+            return;
+        }
+        resetProgress();
+        updateWorkspaces(teamspaces);
+        resetProgress();
+    }
+
+    private boolean getTeamspaceList(HashMap<String, String> teamspaces) {
+        setMessage(Messages.getString("UpdateOperation.1")); //$NON-NLS-1$
+
+        InputStream content = null;
+        try {
+            content = HttpUtil.getContentAsXML(client.getProfileList()
+                    .selectedProfile().getLogin(), client.getProfileList()
+                    .selectedProfile().getPassword(), client.getProfileList()
+                    .selectedProfile().getEndpoint()
+                    + "/teams"); //$NON-NLS-1$
+        } catch (Exception e) {
+            MindClient.showErrorMessage(Messages
+                    .getString("UpdateOperation.2")); //$NON-NLS-1$
+            return false;
+        }
+        // check if some contant was received
+        if (content == null) {
+            return false;
+        }
+        // parse teamspace list
+        SAXReader reader = new SAXReader();
+        Document doc;
+        try {
+            doc = reader.read(content);
+        } catch (DocumentException e) {
+            MindClient.showErrorMessage(Messages
+                    .getString("UpdateOperation.4")); //$NON-NLS-1$
+            return false;
+        }
+        // create a transformer for teamspace list
+        TeamListTransformer listTrans = new TeamListTransformer();
+        listTrans.execute(doc);
+
+        // init progress steps for progress dialog
+        int tsCount = listTrans.getTeamspaces().size();
+        int tsNbr = 0;
+        setProgressSteps(tsCount + 1);
+        updateProgress();
+
+        // loop teamspace descriptions
+        for (String tsID : listTrans.getTeamspaces()) {
+            setMessage(Messages.getString("UpdateOperation.0") + " (" + ++tsNbr + " of "  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+                    + tsCount + ")..."); //$NON-NLS-1$
+
+            content = null;
+            try {
+                content = HttpUtil.getContentAsXML(client.getProfileList()
+                        .selectedProfile().getLogin(), client.getProfileList()
+                        .selectedProfile().getPassword(), client
+                        .getProfileList().selectedProfile().getEndpoint()
+                        + "/teams/team/" + tsID + "/"); //$NON-NLS-1$ //$NON-NLS-2$
+            } catch (Exception e) {
+                MindClient.showErrorMessage(Messages
+                        .getString("UpdateOperation.6") //$NON-NLS-1$
+                        + tsID + Messages.getString("UpdateOperation.7")); //$NON-NLS-1$
+                return false;
+            }
+            // parse teamspace description
+            try {
+                doc = reader.read(content);
+            } catch (DocumentException e) {
+                e.printStackTrace();
+                return false;
+            }
+            // create a transformer for teamspace description
+            TeamspaceTransformer tsTrans = new TeamspaceTransformer();
+            tsTrans.execute(doc);
+
+            teamspaces.put(tsID, tsTrans.getWorkspace());
+            updateProgress();
+        }
+        return true;
+    }
+
+    private void updateWorkspaces(HashMap<String, String> workspaces) {
+        // init SVN client API types
+        svnClient
+                .username(client.getProfileList().selectedProfile().getLogin());
+        svnClient.password(client.getProfileList().selectedProfile()
+                .getPassword());
+
+        // get directory for workspaces
+        File teamspacesDir = new File(client.getProfileList().selectedProfile()
+                .getLocation());
+
+        // check if teamspace dir already exists, if not create it
+        if (!teamspacesDir.exists()) {
+            teamspacesDir.mkdirs();
+        }
+        // init progress steps for progress dialog
+        int tsCount = workspaces.size();
+        int tsNbr = 0;
+        
+        setProgressSteps(tsCount);
+
+        // loop existing workspace directories
+        for (String id : teamspacesDir.list()) {
+            // loop received workspace items
+            if (workspaces.containsKey(id)) {
+                // remove entry from workspace list
+                workspaces.remove(id);
+
+                setMessage(Messages.getString("UpdateOperation.12") + " (" + ++tsNbr + " of "  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+                        + tsCount + ")"); //$NON-NLS-1$
+
+                // update workspace
+                updateWorkspace(new File(teamspacesDir.getAbsolutePath()
+                        + "/" + id), id); //$NON-NLS-1$
+            }
+            updateProgress();
+        }
+        // add additional workspace directories
+        for (String id : workspaces.keySet()) {
+            setMessage(Messages.getString("UpdateOperation.12") + " (" + ++tsNbr + " of "  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+                    + tsCount + ")"); //$NON-NLS-1$
+
+            // create directory for the new workspace
+            File newWorkspaceDir = new File(teamspacesDir.getAbsolutePath()
+                    + "/" + id); //$NON-NLS-1$
+            newWorkspaceDir.mkdir();
+            checkoutWorkspace(workspaces.get(id), newWorkspaceDir, id);
+            updateProgress();
+        }
+    }
+
+    private void checkoutWorkspace(String url, File dir, String id) {
+        try {
+            svnClient.checkout(url, dir.getAbsolutePath(), Revision.HEAD, true);
+        } catch (ClientException e) {
+            MindClient.showErrorMessage(Messages
+                    .getString("UpdateOperation.11") //$NON-NLS-1$
+                    + " '" //$NON-NLS-1$
+                    + id + "' (" //$NON-NLS-1$
+                    + e.getMessage() + ")"); //$NON-NLS-1$
+        }
+    }
+
+    private void updateWorkspace(File dir, String id) {
+        try {
+            svnClient.update(dir.getAbsolutePath(), Revision.HEAD, true);
+        } catch (ClientException e) {
+            MindClient.showErrorMessage(Messages
+                    .getString("UpdateOperation.11") //$NON-NLS-1$
+                    + " '" //$NON-NLS-1$
+                    + id + "' (" //$NON-NLS-1$
+                    + e.getMessage() + ")"); //$NON-NLS-1$
+        }
+    }
+}
