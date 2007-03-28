c3ddeed5ea89e874d9239a239309471d4708ebdd
lars <lars@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-03-28 20:43:00 +0000

    added correct helper class

--- mindquarry-mac-java/src/main/java/com/mindquarry/desktop/workspace/MacSVNHelper.java
+++ mindquarry-mac-java/src/main/java/com/mindquarry/desktop/workspace/MacSVNHelper.java
@@ -1,20 +1,10 @@
-/*
- * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- */
 package com.mindquarry.desktop.workspace;
+
 
 import org.tigris.subversion.javahl.NotifyInformation;
 import org.tigris.subversion.javahl.Status;
+
+import com.mindquarry.desktop.workspace.SVNHelper;
 
 /**
  * Specilization of the {@link SVNHelper} for Mac OS related clients.
