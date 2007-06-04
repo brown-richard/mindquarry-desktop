e64290f730fa0225850282ce7e48f57753c2605d
daniel <daniel@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-06-04 08:40:30 +0000

    fix delay of notification widget on first start

--- mindquarry-desktop-client/src/main/java/com/mindquarry/desktop/client/MindClient.java
+++ mindquarry-desktop-client/src/main/java/com/mindquarry/desktop/client/MindClient.java
@@ -413,8 +413,7 @@ public class MindClient {
             // at first start the store returns 0 which doesn't make sense, so use a default:
             delay = GeneralSettingsPage.DEFAULT_NOTIFY_DELAY;
         }
-        widget.show(title, message, getPreferenceStore().getInt(
-                GeneralSettingsPage.NOTIFY_DELAY) * 1000);
+        widget.show(title, message, delay * 1000);
         widget.dispose();
     }
 }
