30c839c42b26f008a4687a2b27d53e97570b454c
daniel <daniel@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-06-04 08:49:12 +0000

    press return = OK, not cancel

--- trunk/mindquarry-desktop-client/src/main/java/com/mindquarry/desktop/client/task/dialog/TaskDialog.java
+++ trunk/mindquarry-desktop-client/src/main/java/com/mindquarry/desktop/client/task/dialog/TaskDialog.java
@@ -197,7 +197,7 @@ public class TaskDialog extends TitleAreaDialog {
         createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
                 true);
         createButton(parent, IDialogConstants.CANCEL_ID,
-                IDialogConstants.CANCEL_LABEL, true);
+                IDialogConstants.CANCEL_LABEL, false);
     }
 
     private void initTask() {
