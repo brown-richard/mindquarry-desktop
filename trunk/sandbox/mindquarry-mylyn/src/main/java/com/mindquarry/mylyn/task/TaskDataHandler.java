18f3c5d7a45bb281d1b8fc8f4809235829cffeb4
alexs <alexs@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-08-01 11:09:46 +0000

    added task data handler stub

---
+++ trunk/sandbox/mindquarry-mylyn/src/main/java/com/mindquarry/mylyn/task/TaskDataHandler.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.mylyn.task;
+
+import java.util.Collections;
+import java.util.Set;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.mylyn.tasks.core.AbstractAttributeFactory;
+import org.eclipse.mylyn.tasks.core.AbstractTaskDataHandler;
+import org.eclipse.mylyn.tasks.core.RepositoryTaskData;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
+
+import com.mindquarry.mylyn.repository.RepositoryConnector;
+
+/**
+ * Add summary documentation here.
+ *
+ * @author <a href="mailto:saar@mindquarry.com">Alexander Saar</a>
+ */
+public class TaskDataHandler extends AbstractTaskDataHandler {
+	private RepositoryConnector connector;
+
+	public TaskDataHandler(RepositoryConnector connector) {
+		this.connector = connector;
+	}
+
+	@Override
+	public Set<String> getSubTaskIds(RepositoryTaskData taskData) {
+		return Collections.emptySet();
+	}
+
+	@Override
+	public AbstractAttributeFactory getAttributeFactory(String repositoryUrl,
+			String repositoryKind, String taskKind) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AbstractAttributeFactory getAttributeFactory(
+			RepositoryTaskData taskData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RepositoryTaskData getTaskData(TaskRepository repository,
+			String taskId, IProgressMonitor monitor) throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean initializeTaskData(TaskRepository repository,
+			RepositoryTaskData data, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String postTaskData(TaskRepository repository,
+			RepositoryTaskData taskData, IProgressMonitor monitor)
+			throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
