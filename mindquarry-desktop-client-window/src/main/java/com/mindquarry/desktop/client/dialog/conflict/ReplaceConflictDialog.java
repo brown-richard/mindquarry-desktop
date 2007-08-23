70c91ef30132fe83a69294e632104b6b4fc9294e
victor <victor@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-08-23 11:49:33 +0000

    added new base class for conflicts with a rename action added new dialogs for replace and obstructed conflicts implemented corresponding methods in client config handler

---
+++ mindquarry-desktop-client-window/src/main/java/com/mindquarry/desktop/client/dialog/conflict/ReplaceConflictDialog.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2006-2007 Mindquarry GmbH, All Rights Reserved
+ * 
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ */
+package com.mindquarry.desktop.client.dialog.conflict;
+
+import org.apache.commons.io.FilenameUtils;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.FocusEvent;
+import org.eclipse.swt.events.FocusListener;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.tigris.subversion.javahl.ClientException;
+
+import com.mindquarry.desktop.client.Messages;
+import com.mindquarry.desktop.workspace.conflict.ReplaceConflict;
+
+/**
+ * Dialog for resolving replace conflicts.
+ * 
+ * @author <a href="mailto:victor(dot)saar(at)mindquarry(dot)com">Victor Saar</a>
+ */
+public class ReplaceConflictDialog extends AbstractConflictDialog {
+
+    private String newName;
+    private Text newNameField;
+    private ReplaceConflict conflict;
+    private ReplaceConflict.Action resolveMethod;
+
+    private static final ReplaceConflict.Action DEFAULT_RESOLUTION = ReplaceConflict.Action.RENAME;
+    
+    public ReplaceConflictDialog(ReplaceConflict conflict, Shell shell) {
+        super(shell);
+        this.conflict = conflict;
+        resolveMethod = DEFAULT_RESOLUTION;
+    }
+
+    protected void showFileInformation(Composite composite) {
+        Label name = new Label(composite, SWT.READ_ONLY);
+        name.setText(Messages.getString("Filename(s)") + ": " + conflict.getStatus().getPath());
+    }
+
+    @Override
+    protected String getMessage() {
+        // FIXME no text so far
+        return Messages.getString("Add text here.");
+    }
+
+    @Override
+    protected void createLowerDialogArea(Composite composite) {
+        Composite subComposite = new Composite(composite, SWT.NONE);
+        subComposite.setLayout(new GridLayout(2, false));
+        Button button1 = makeRadioButton(subComposite,
+                Messages.getString("Rename file and upload it under the new name"),  //$NON-NLS-1$
+                ReplaceConflict.Action.RENAME);
+        button1.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+                newNameField.setEnabled(true);
+            }
+        });
+                
+        newNameField = new Text(subComposite, SWT.BORDER | SWT.SINGLE);
+        // TODO: make field wider
+        String oldName = FilenameUtils.getName(conflict.getStatus().getPath());
+        String nameSuggestion = getNameSuggestion(oldName);
+        newNameChanged(nameSuggestion);
+        newNameField.setText(nameSuggestion);
+        newName = nameSuggestion;
+        newNameField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent arg0) {
+                newNameField.selectAll();
+            }
+            public void focusLost(FocusEvent arg0) {
+            }
+        });
+        newNameField.addKeyListener(new KeyListener() {
+            public void keyPressed(KeyEvent arg0) {
+            }
+            public void keyReleased(KeyEvent arg0) {
+                newNameChanged(newNameField.getText());
+            }
+        });
+        newNameField.setFocus();
+        /*Button button2 = makeRadioButton(subComposite,
+                // FIXME: passt nicht zusammen, sollen wir wirklich replace anbieten?!?!
+                // das w??rde lokale ??nderungen ??berschreiben
+                //Messages.getString("Overwrite the file on the server with your local version"),  //$NON-NLS-1$
+                //Action.REPLACE);
+                Messages.getString("Overwrite your local file with the one from the server"),  //$NON-NLS-1$
+                Action.REPLACE);
+        button2.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+                newNameField.setEnabled(false);
+                okButton.setEnabled(true);
+            }
+        });*/
+    }
+
+    private void newNameChanged(String name) {
+        try {
+            if (conflict.isRenamePossible(name)) {
+                newName = newNameField.getText();
+                if (okButton != null) {
+                    okButton.setEnabled(true);
+                }
+                System.err.println(name + " accepted");
+            } else {
+                // TODO: show error
+                if (okButton != null) {
+                    okButton.setEnabled(false);
+                }
+                System.err.println(name + " not accepted");
+            }
+        } catch (ClientException e) {
+            throw new RuntimeException(e.toString(), e);
+        }
+    }
+    
+    private String getNameSuggestion(String existingName) {
+        int pos = existingName.lastIndexOf('.');
+        // TODO: avoid suggesting a name that exists
+        if (pos == -1) {
+            return existingName + "_1";
+        } else {
+            return existingName.substring(0, pos) + "_1" + existingName.substring(pos);
+        }
+    }
+
+    public String getNewName() {
+        return newName;
+    }
+    
+    protected Button makeRadioButton(Composite composite, String text, final ReplaceConflict.Action action) {
+        Button button = new Button(composite, SWT.RADIO);
+        button.setText(text);
+        if (action == DEFAULT_RESOLUTION) {
+            button.setSelection(true);
+        }
+        button.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_FILL));
+        button.addListener(SWT.Selection, new Listener() {
+            public void handleEvent(Event event) {
+                resolveMethod = action;
+            }
+        });
+        return button;
+    }
+
+    public ReplaceConflict.Action getResolveMethod() {
+        return resolveMethod;
+    }
+
+}
--- mindquarry-desktop-commons/src/main/java/com/mindquarry/desktop/workspace/conflict/AddConflict.java
+++ mindquarry-desktop-commons/src/main/java/com/mindquarry/desktop/workspace/conflict/AddConflict.java
@@ -15,7 +15,6 @@ package com.mindquarry.desktop.workspace.conflict;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -32,7 +31,7 @@ import com.mindquarry.desktop.workspace.exception.CancelException;
  * @author <a href="mailto:victor.saar@mindquarry.com">Victor Saar</a>
  * @author <a href="mailto:klimetschek@mindquarry.com">Alexander Klimetschek</a>
  */
-public class AddConflict extends Conflict {
+public class AddConflict extends RenamingConflict {
 	private Action action = Action.UNKNOWN;
 	
 	public enum Action {
@@ -54,17 +53,6 @@ public class AddConflict extends Conflict {
     private List<Status> localAdded;
     private List<Status> remoteAdded;
     
-    /**
-     * The folder in which the added object (status) lies in. Needed to check
-     * for rename conflicts.
-     */
-    private File folder;
-    /**
-     * All objects that are added remotely in the same folder as the object to
-     * be able to check for rename conflicts.
-     */
-    private List<String> remoteAddedInFolder = null;
-	
 	public AddConflict(Status status, List<Status> localAdded, List<Status> remoteAdded) {
 		super(status);
 		this.localAdded = localAdded;
@@ -132,53 +120,6 @@ public class AddConflict extends Conflict {
 		handler.handle(this);
 	}
 	
-	/**
-	 * Call this during conflict resolving before a call to doRename() to check
-	 * if the given name is possible without another conflict. Not possible when
-	 * another file or folder exists in the same directory or when a file or
-	 * folder with that name will be added during the next update or when the
-	 * filename is null or whitespace only.
-	 * 
-	 * @param newName the new name for the conflicted file/folder to check for
-	 * @return true if the name can be used, false if not
-	 * @throws ClientException this method must check the svn client for info
-	 * about newly added files in the next update; simply catch that exception
-	 * and throw a CancelException in the handle method
-	 */
-	public boolean isRenamePossible(String newName) throws ClientException {
-        if (newName == null) {
-            return false;
-        }
-        if (newName.trim().equals("")) {
-            return false;
-        }
-
-        if (remoteAddedInFolder == null) {
-	        // lazily retrieve possible conflicts with other remotely added files
-	        remoteAddedInFolder = new ArrayList<String>();
-	        for (Status s : client.status(folder.getAbsolutePath(), true, true, false)) {
-	            // simply add all files that are either locally or remotely in
-	            // a non-normal state - this will include delete cases, but
-	            // those files exist locally anyway until the update is done
-	            remoteAddedInFolder.add(new File(s.getPath()).getName());
-	        }
-	    }
-	    
-	    // TODO: remember all choosen newNames for a certain directory in a
-	    // static list and check against them (see above when renameTo fails)
-	    
-	    // TODO: check for relative path names (eg. ../../newfile) and return false
-	    
-        // such a file must not exist locally yet and it must not be added
-        // during the next update (that would be another conflict then)
-	    boolean result = !(new File(folder, newName).exists() || remoteAddedInFolder.contains(newName));
-	    
-	    if (!result) {
-	        System.out.println("Cannot rename to '" + newName + "'");
-	    }
-	    return result;
-	}
-
 	public void doRename(String newName) {
 	    // TODO: double check here? what if it fails? throw exception? return false?
 //	    if (!isRenamePossible(newName)) {
