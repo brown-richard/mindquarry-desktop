c9719caee9b8d9f22fa719d706474511e7b39dd0
daniel <daniel@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-08-23 13:31:34 +0000

    cleanup

--- trunk/mindquarry-desktop-client-window/src/main/java/com/mindquarry/desktop/client/widget/workspace/WorkspaceUpdateContainerRunnable.java
+++ trunk/mindquarry-desktop-client-window/src/main/java/com/mindquarry/desktop/client/widget/workspace/WorkspaceUpdateContainerRunnable.java
@@ -28,7 +28,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.tigris.subversion.javahl.StatusKind;
 
