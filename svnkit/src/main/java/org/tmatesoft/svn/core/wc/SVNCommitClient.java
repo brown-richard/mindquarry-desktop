a493583e83205c0f17c4073ec4905191ecf73699
alex <alex@bb3dd236-d552-0410-9882-75ba70382fc5>
2007-08-29 08:51:20 +0000

    added patch from svnkit revision 2885 to fix problem on Mac with umlaute (UTF-8 decomposed) but using java.text.Normalizer instead of a manual decompose method in the patch

--- svnkit/src/main/java/org/tmatesoft/svn/core/wc/SVNCommitClient.java
+++ svnkit/src/main/java/org/tmatesoft/svn/core/wc/SVNCommitClient.java
@@ -41,6 +41,7 @@ import org.tmatesoft.svn.core.internal.wc.SVNCommitUtil;
 import org.tmatesoft.svn.core.internal.wc.SVNCommitter;
 import org.tmatesoft.svn.core.internal.wc.SVNErrorManager;
 import org.tmatesoft.svn.core.internal.wc.SVNEventFactory;
+import org.tmatesoft.svn.core.internal.wc.SVNFileListUtil;
 import org.tmatesoft.svn.core.internal.wc.SVNFileType;
 import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
 import org.tmatesoft.svn.core.internal.wc.SVNImportMediator;
@@ -980,7 +981,7 @@ public class SVNCommitClient extends SVNBasicClient {
 
     private boolean importDir(SVNDeltaGenerator deltaGenerator, File rootFile, File dir, String importPath, boolean useGlobalIgnores, boolean recursive, ISVNEditor editor) throws SVNException {
         checkCancelled();
-        File[] children = dir.listFiles();
+        File[] children = SVNFileListUtil.listFiles(dir);
         boolean changed = false;
         for (int i = 0; children != null && i < children.length; i++) {
             File file = children[i];
